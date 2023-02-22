aaa4056ae88d3f54dcc68521f3746b430e4c9eef
DinoHunter696 <55260008+DinoHunter696@users.noreply.github.com>
2023-02-22 12:45:17 -0800

    tttesr

--- src/main/java/frc/robot/commands/LockToGamePiece.java
+++ src/main/java/frc/robot/commands/LockToGamePiece.java
@@ -1,32 +1,87 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
+import frc.robot.Constants;
+import frc.robot.subsystems.Swerve;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+
 public class LockToGamePiece extends CommandBase {
-  /** Creates a new LockToGamePiece. */
-  public LockToGamePiece() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+    private double rotation;
+    private Translation2d translation;
+    private boolean fieldRelative;
+    private boolean openLoop;
+    
+    private Swerve s_Swerve;
+    private Joystick controller;
+    private int translationAxis;
+    private int strafeAxis;
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
+    private int pipeline;
+    private double mapdouble(double x, double in_min, double in_max, double out_min, double out_max){
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+    /**
+     * Driver control
+     */
+    public LockToGamePiece(Swerve s_Swerve, Joystick controller, int translationAxis, int strafeAxis,  boolean fieldRelative, boolean openLoop, int pipeline) {
+        this.s_Swerve = s_Swerve;
+        addRequirements(s_Swerve);
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+        this.controller = controller;
+        this.translationAxis = translationAxis;
+        this.strafeAxis = strafeAxis;
+        this.fieldRelative = fieldRelative;
+        this.openLoop = openLoop;
+        this.pipeline = pipeline;
+    }
+
+    @Override
+    public void execute() {
+        double yAxis = -controller.getRawAxis(translationAxis);
+        double xAxis = controller.getRawAxis(strafeAxis);
+        double rAxis = s_Swerve.frontCamOffset(pipeline);
+        
+        /* Deadbands */
+
+        if (Math.abs(yAxis) > Constants.stickDeadband) {
+            if (yAxis > 0){
+                yAxis = mapdouble(yAxis, Constants.stickDeadband, 1, 0, 1);
+            } else {
+                yAxis = mapdouble(yAxis, -Constants.stickDeadband, -1, 0, -1);
+            }
+        }
+        else{
+            yAxis = 0;
+        }
+        
+        if (Math.abs(xAxis) > Constants.stickDeadband) {
+            if (xAxis > 0){
+                xAxis = mapdouble(xAxis, Constants.stickDeadband, 1, 0, 1);
+            } else {
+                xAxis = mapdouble(xAxis, -Constants.stickDeadband, -1, 0, -1);
+            }
+        }
+        else{
+            xAxis = 0;
+        }
+
+        if (Math.abs(rAxis) > Constants.stickDeadband) {
+            if (rAxis > 0){
+                rAxis = mapdouble(rAxis, Constants.stickDeadband, 1, 0, 1);
+            } else {
+                rAxis = mapdouble(rAxis, -Constants.stickDeadband, -1, 0, -1);
+            }
+        }
+        else{
+            rAxis = 0;
+        }
+
+        translation = new Translation2d(yAxis, xAxis).times(Constants.Swerve.maxSpeed);
+        rotation = rAxis * Constants.Swerve.maxAngularVelocity;
+        s_Swerve.drive(translation, rotation, fieldRelative, openLoop);
+    }
 }
